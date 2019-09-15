188e6bf2338516f446148db426235791986af05b
gamsteron <gamsteron@gmail.com>
2019-09-15 15:55:40 +0200

    url

--- AIO/GamsteronAIO.lua
+++ AIO/GamsteronAIO.lua
@@ -10,7 +10,7 @@ end
 do
     local path = COMMON_PATH.."GamsteronAIO_Loader.lua"
     if FileExist(path) then require("GamsteronAIO_Loader") return end
-    DownloadFileAsync("https://raw.githubusercontent.com/gamsteron/Gamsteron/AIO/master/GamsteronAIO_Loader.lua", path, function() end)
+    DownloadFileAsync("https://raw.githubusercontent.com/gamsteron/Gamsteron/master/AIO/GamsteronAIO_Loader.lua", path, function() end)
     while not FileExist(path) do end
     require("GamsteronAIO_Loader")
 end
