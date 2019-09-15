945893269807b535699b14c24e9297cdc17639f6
gamsteron <gamsteron@gmail.com>
2019-09-15 13:13:02 +0200

    loader, int downloader

--- Gamsteron_Version.lua
+++ Gamsteron_Version.lua
@@ -1,7 +1,7 @@
 return {
 Gamsteron_Activator=1.2,
 Gamsteron_Arrays=1.2,
-Gamsteron_Loader=1.9,
+Gamsteron_Loader=2.0,
 Gamsteron_ObjectManager=1.2,
 Gamsteron_Orbwalker=1.1,
 Gamsteron_Utilities=1.2,}
